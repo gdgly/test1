2682f1c86b8a3deaddb17d4304b0f867ded80018
dannywang <dannywang@zhytek.com>
2019-07-22 16:14:31 +0800

    Import 首次默认创建

---
+++ EarBuds/apps/fw/src/core/memprot/memprot.c
@@ -0,0 +1,19 @@
+/* Copyright (c) 2016 Qualcomm Technologies International, Ltd. */
+/*    */
+
+#include "memprot/memprot.h"
+
+
+
+void memprot_enable(void)
+{
+
+
+}
+
+void memprot_disable(void)
+{
+}
+
+
+
