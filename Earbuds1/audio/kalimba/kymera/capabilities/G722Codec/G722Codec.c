9a45690b7e84f45d0791af293ead51451ad241e1
dannywang <dannywang@zhytek.com>
2019-09-16 08:41:35 +0800

    G722 Read moer times

--- Earbuds1/audio/kalimba/kymera/capabilities/G722Codec/G722Codec.c
+++ Earbuds1/audio/kalimba/kymera/capabilities/G722Codec/G722Codec.c
@@ -14,10 +14,17 @@
 #include "G722Codec.h"
 #include "codec\acodec.h"
 
+#if 0
 #define G722_DEBUG       L2_DBG_MSG
 #define G722_DEBUG1      L2_DBG_MSG1
 #define G722_DEBUG2      L2_DBG_MSG2
 #define G722_DEBUG3      L2_DBG_MSG3
+#else
+#define G722_DEBUG(...)
+#define G722_DEBUG1(...)
+#define G722_DEBUG2(...)
+#define G722_DEBUG3(...)
+#endif
 
 // #define TEST_NO_ENC       // 测试,类似于 PASS through，可以正常多次拨打电话
 
@@ -61,6 +68,7 @@ const opmsg_handler_lookup_table_entry G722Codec_opmsg_handler_table[] =
     {OPMSG_COMMON_ID_SET_BUFFER_SIZE,            G722Codec_opmsg_set_buffer_size},
     {OPMSG_COMMON_ID_GET_PARAMS,                   G722Codec_opmsg_get_params},
     {OPMSG_COMMON_ID_SET_PARAMS,                   G722Codec_opmsg_set_params},
+    {OPMSG_PASSTHROUGH_ID_DISABLE_AUDIO_FORWARD,   G722Codec_opmsg_disable_audio_forward},
     {0, NULL}
 };
 
@@ -86,9 +94,10 @@ static inline G722CODEC_OP_DATA *get_instance_data(OPERATOR_DATA *op_data)
     return (G722CODEC_OP_DATA *) base_op_get_instance_data(op_data);
 }
 
-static signed G722Codec_metadata_strict_transport(G722CODEC_OP_DATA *opx_data, tCbuffer *src, tCbuffer *dst, unsigned octets)
+
+static signed G722Codec_metadata_read(G722CODEC_OP_DATA *opx_data, tCbuffer *src, unsigned octets)
 {
-    metadata_tag *ret_mtag = NULL, *new_mtag = NULL;
+    metadata_tag *ret_mtag = NULL;
     unsigned b4idx, afteridx;
 
     patch_fn_shared(buff_metadata);
@@ -98,8 +107,19 @@ static signed G722Codec_metadata_strict_transport(G722CODEC_OP_DATA *opx_data, t
         return -1;
     }
 
-    ret_mtag = buff_metadata_remove(src, octets*OCTETS_PER_SAMPLE, &b4idx, &afteridx);    
+    ret_mtag = buff_metadata_remove(src, octets*OCTETS_PER_SAMPLE, &b4idx, &afteridx);
 //    G722_DEBUG3("get ret_mtag=%p %d %d", ret_mtag, b4idx, afteridx);
+    buff_metadata_tag_list_delete(ret_mtag);
+
+    cbuffer_read(opx_data->ip_buffer, (int*)&opx_data->in_buffer[opx_data->rd_samples], octets);
+    opx_data->rd_samples += (unsigned short)octets;
+    return 0;
+}
+
+
+static signed G722Codec_metadata_write(G722CODEC_OP_DATA *opx_data, tCbuffer *dst)
+{
+    metadata_tag *new_mtag = NULL;
 
     new_mtag = buff_metadata_new_tag();
     if(!new_mtag) {
@@ -109,31 +129,22 @@ static signed G722Codec_metadata_strict_transport(G722CODEC_OP_DATA *opx_data, t
         METADATA_PACKET_START_SET(new_mtag);
         METADATA_PACKET_END_SET(new_mtag);
 #ifdef TEST_NO_ENC
-        new_mtag->length = octets * OCTETS_PER_SAMPLE;
+        new_mtag->length = px_data->in_samples * OCTETS_PER_SAMPLE;
 #else
         new_mtag->length = opx_data->out_samples * G722_BYTE_PER_SAMPLE;        // bytePERsample = 2
 #endif
 
-        if (ret_mtag)  {            // 有metadata_tag的情况
-            new_mtag->timestamp = ret_mtag->timestamp;
-            new_mtag->sp_adjust = ret_mtag->sp_adjust;
-        }
-        else {                      // 无metadata_tag的情况
-        }
-
+//        new_mtag->timestamp = ret_mtag->timestamp;
+//        new_mtag->sp_adjust = ret_mtag->sp_adjust;
         buff_metadata_append(dst, new_mtag, 0, new_mtag->length);
     }
 
-    buff_metadata_tag_list_delete(ret_mtag);
     return 0;
 }
-
 
 static void G722Codec_processing_encode(G722CODEC_OP_DATA *opx_data, unsigned octets)
 {
     signed short i, *ptr, outsize = G722_ENCODE_OUT_SAMPLES * G722_BYTE_PER_SAMPLE;
-
-    cbuffer_read (opx_data->ip_buffer, (int*)opx_data->in_buffer, opx_data->in_samples);
 
 #ifdef TEST_NO_ENC
     (void)i; (void)ptr; (void)outsize;
@@ -143,57 +154,73 @@ static void G722Codec_processing_encode(G722CODEC_OP_DATA *opx_data, unsigned oc
                    (unsigned char*)opx_data->out_buffer, (unsigned short*)&outsize);
 
 #else
+    // 将每个SAMPLE为4个字节转换为2个字节
     ptr = (signed short *)opx_data->in_buffer;
     for(i = 0; i < octets; i++) {
      //  ptr[i] = ptr[i*2];   // 在前端为模拟音频时我们是取 ptr[i*2]
            ptr[i] = ptr[i*2+1]; // 在前端为SPLITER 时我们是取 ptr[i*2+1]
     }
-    L2_DBG_MSG1("encode intime=%d", time_get_time());
+    G722_DEBUG1("encode intime=%d", time_get_time());
 
     acodec_encoder(opx_data->g722Handle, (unsigned char*)opx_data->in_buffer, (short)(opx_data->in_samples*G722_BYTE_PER_SAMPLE),
                    (unsigned char*)opx_data->out_buffer, (unsigned short*)&outsize);
 
+    // 将每个SAMPLE为2个字节转换为4个字节
     ptr = (signed short *)opx_data->out_buffer;
     for(i = (signed short)(opx_data->out_samples-1); i >= 0; i--)
         ptr[i*2] = ptr[i];
 
     cbuffer_write(opx_data->op_buffer, (int*)opx_data->out_buffer, opx_data->out_samples);
-    L2_DBG_MSG1("encode outtime=%d", time_get_time());
+    G722_DEBUG1("encode outtime=%d", time_get_time());
 #endif
 }
 
 
 /* Data processing function */
+// 使用有多少数据先读取出来，然后再进行编码
 static void G722Codec_process_data(OPERATOR_DATA *op_data, TOUCHED_TERMINALS *touched)
 {
     G722CODEC_OP_DATA *opx_data = get_instance_data(op_data);
-    unsigned data_to_process, in_data, output_space;
+    unsigned in_data, output_space, max_read;
+
+    // 本次最多读取数据量
+    max_read = opx_data->in_samples - opx_data->rd_samples;
 
     in_data = cbuffer_calc_amount_data_in_words(opx_data->ip_buffer);
-    if(in_data < opx_data->in_samples)
-        return;
+    if(in_data < 1) return;
 
-    data_to_process = opx_data->in_samples;       // 每次取 sample
-    output_space = cbuffer_calc_amount_space_in_words(opx_data->op_buffer);
+    if(max_read > in_data)
+        max_read = in_data;
 
-    G722_DEBUG2("G722Codec_process in=%d out=%d", in_data, output_space);
-    if (output_space < opx_data->out_samples) {
+    G722Codec_metadata_read(opx_data, opx_data->ip_buffer, max_read);
+
+    if(opx_data->disable_audforward) {
+        opx_data->rd_samples = 0;
         touched->sources = TOUCHED_SOURCE_0;
         return;
+    }
+
+    output_space = cbuffer_calc_amount_space_in_words(opx_data->op_buffer);
+    G722_DEBUG3("G722Codec_process in=%d/%d read=%d", in_data, max_read, opx_data->rd_samples);
+    if (output_space < opx_data->out_samples) {
+        touched->sinks   = TOUCHED_SINK_0;
+        return;
+    }
+
+    if(opx_data->rd_samples >= opx_data->in_samples) {    // only support ==
+        G722Codec_processing_encode(opx_data, opx_data->in_samples);
+        G722Codec_metadata_write(opx_data, opx_data->op_buffer);
+        opx_data->rd_samples = 0;
     }
 
     /* We have got this far, so we have something to do on every output */
     touched->sources = TOUCHED_SOURCE_0;
     touched->sinks   = TOUCHED_SINK_0;
-
-    G722Codec_metadata_strict_transport(opx_data, opx_data->ip_buffer, opx_data->op_buffer, data_to_process);
-
-    G722Codec_processing_encode(opx_data, data_to_process);
 }
 
 static void G722Codec_setmode(G722CODEC_OP_DATA *opx_data, bool is_encoder)
 {
-    opx_data->is_encoder       = is_encoder;
+    opx_data->is_encoder           = (unsigned char)is_encoder;
     if(is_encoder) {
         opx_data->in_samples       = G722_ENCODE_IN_SAMPLES;
         opx_data->out_samples      = G722_ENCODE_IN_SAMPLES / 16;
@@ -210,6 +237,24 @@ static void G722Codec_setmode(G722CODEC_OP_DATA *opx_data, bool is_encoder)
 #endif
 }
 
+static void G722Codec_Init(G722CODEC_OP_DATA *opx_data)
+{
+    opx_data->sample_rate      = 16000;
+    opx_data->ip_format        = AUDIO_DATA_FORMAT_FIXP;
+    opx_data->op_format        = AUDIO_DATA_FORMAT_16_BIT;
+
+    opx_data->num_active_chans = 0;
+    opx_data->active_chans     = 0;
+    opx_data->disable_audforward=0;
+
+    opx_data->sample_cnt       = 0;
+    opx_data->timestamp        = 0; // time_get_time();
+    G722Codec_setmode(opx_data, 1);
+    opx_data->in_buffer        = (unsigned *)xzpmalloc(opx_data->in_samples * 4);
+    opx_data->out_buffer       = (unsigned *)xzpmalloc(opx_data->out_samples *4);
+    opx_data->rd_samples       = 0;
+}
+
 bool G722Codec_create(OPERATOR_DATA *op_data, void *message_data, unsigned *response_id, void **response_data)
 {
     G722CODEC_OP_DATA *opx_data = get_instance_data(op_data);
@@ -220,21 +265,7 @@ bool G722Codec_create(OPERATOR_DATA *op_data, void *message_data, unsigned *resp
         return FALSE;
     }
 
-    opx_data->sample_rate      = 16000;
-    opx_data->ip_format = AUDIO_DATA_FORMAT_16_BIT;
-    opx_data->ip_format = AUDIO_DATA_FORMAT_FIXP;
-
-    opx_data->op_format = AUDIO_DATA_FORMAT_16_BIT;
-
-    opx_data->num_active_chans = 0;
-    opx_data->active_chans     = 0;
-
-    opx_data->sample_cnt       = 0;
-    opx_data->timestamp        = 0; // time_get_time();
-    G722Codec_setmode(opx_data, 1);
-    opx_data->in_buffer        = (unsigned *)xzpmalloc(opx_data->in_samples * 4);
-    opx_data->out_buffer       = (unsigned *)xzpmalloc(opx_data->out_samples *4);
-
+    G722Codec_Init(opx_data);
 #ifdef INSTALL_TTP
     {
         /* Allocate and initialise the TTP data */
@@ -273,7 +304,7 @@ bool G722Codec_destroy(OPERATOR_DATA *op_data, void *message_data, unsigned *res
     pdelete(opx_data->in_buffer), opx_data->in_buffer = NULL;
     pdelete(opx_data->out_buffer), opx_data->out_buffer = NULL;
     pdelete(opx_data->g722Handle), opx_data->g722Handle = NULL;
-    L2_DBG_MSG1("G722Codec_destroy %d", __LINE__);
+    L2_DBG_MSG("G722Codec_destroy");
 
 #ifdef INSTALL_TTP
     /* Time to play pointer will only be non-NULL if this is the TTP_PASS capability
@@ -371,7 +402,7 @@ bool G722Codec_connect(OPERATOR_DATA *op_data, void *message_data, unsigned *res
             if (opx_data->metadata_ip_buffer == NULL)   {
                 if (buff_has_metadata(opx_data->ip_buffer))     {
                     opx_data->metadata_ip_buffer = opx_data->ip_buffer;
-                    L2_DBG_MSG2("ipbuf size=%d ptr=%p\n", opx_data->metadata_ip_buffer->size,
+                    L2_DBG_MSG2("G722Codec_connect ipbuf size=%d ptr=%p", opx_data->metadata_ip_buffer->size,
                         opx_data->metadata_ip_buffer->base_addr);
                 }
             }
@@ -392,7 +423,7 @@ bool G722Codec_connect(OPERATOR_DATA *op_data, void *message_data, unsigned *res
 #ifdef TEST_NO_ENC
                     cbuffer_set_usable_octets(opx_data->metadata_op_buffer, 4);
 #endif
-                    L2_DBG_MSG2("opbuf size=%d ptr=%p\n", opx_data->metadata_op_buffer->size,
+                    L2_DBG_MSG2("G722Codec_connect opbuf size=%d ptr=%p", opx_data->metadata_op_buffer->size,
                         opx_data->metadata_op_buffer->base_addr);
                 }
             }
@@ -437,7 +468,7 @@ bool G722Codec_disconnect(OPERATOR_DATA *op_data, void *message_data, unsigned *
         }
     }
 
-    L2_DBG_MSG1("G722Codec_disconnect %d", __LINE__);
+    L2_DBG_MSG("G722Codec_disconnect %d");
 
     return TRUE;
 }
@@ -446,12 +477,13 @@ bool G722Codec_reset(OPERATOR_DATA *op_data, void *message_data, unsigned *respo
 {
     G722CODEC_OP_DATA *opx_data = get_instance_data(op_data);
 
+    G722Codec_Init(opx_data);
     acodec_reset(opx_data->g722Handle);
     if (!base_op_reset(op_data, message_data, response_id, response_data)) {
         return FALSE;
     }
 
-    L2_DBG_MSG1("G722Codec_reset %d", __LINE__);
+    L2_DBG_MSG("G722Codec_reset");
     return TRUE;
 }
 
@@ -514,6 +546,17 @@ bool G722Codec_opmsg_output_data_type(OPERATOR_DATA *op_data, void *message_data
      return TRUE;
 }
 
+/*
+ * basic_passthrough_disable_audio_forward
+ */
+bool G722Codec_opmsg_disable_audio_forward(OPERATOR_DATA *op_data, void *message_data,
+                                             unsigned *resp_length, OP_OPMSG_RSP_PAYLOAD **resp_data)
+{
+    G722CODEC_OP_DATA *opx_data = get_instance_data(op_data);
 
+    opx_data->disable_audforward = (bool)OPMSG_FIELD_GET(message_data, OPMSG_PASSTHROUGH_DISABLE_AUDIO_FORWARD, DISABLE);
+
+   return TRUE;
+}
 
 
