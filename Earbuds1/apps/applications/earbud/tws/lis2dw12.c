9bb49659f9ca22d2435e3d2bad8ad375fd821d95
jyy <jiaoyunyi@zhytek.com>
2019-10-14 09:45:51 +0800

    添加lis2dw12 tap功能代码

---
+++ Earbuds1/apps/applications/earbud/tws/lis2dw12.c
@@ -0,0 +1,148 @@
+#include "lis2dw12.h"
+#ifdef HAVE_LIS2DW12
+
+/*! \brief Read a register from the proximity sensor */
+bool lis2dw12ReadRegister(bitserial_handle handle, uint8 reg,  uint8 *value)
+{
+    bitserial_result result;
+    /* First write the register address to be read */
+    result = BitserialWrite(handle,
+                            BITSERIAL_NO_MSG,
+                            &reg, 1,
+                            BITSERIAL_FLAG_BLOCK);
+    if (result == BITSERIAL_RESULT_SUCCESS){
+        /* Now read the actual data in the register */
+        result = BitserialRead(handle,
+                                BITSERIAL_NO_MSG,
+                                value, 1,
+                                BITSERIAL_FLAG_BLOCK);
+    }
+    if(result != BITSERIAL_RESULT_SUCCESS){
+        //printf("%s faild,result = %d\n",__func__, result);
+    }
+    return (result == BITSERIAL_RESULT_SUCCESS);
+}
+
+/*! \brief Write to a proximity sensor register */
+bool lis2dw12WriteRegister(bitserial_handle handle, uint8 reg, uint8 value)
+{
+    bitserial_result result;
+    uint8 command[2] = {reg, value};
+
+    /* Write the write command and register */
+    result = BitserialWrite(handle,
+                            BITSERIAL_NO_MSG,
+                            command, 2,
+                            BITSERIAL_FLAG_BLOCK);
+    if(result != BITSERIAL_RESULT_SUCCESS){
+        printf("%s faild,result = %d\n",__func__, result);
+    }
+    return (result == BITSERIAL_RESULT_SUCCESS);
+}
+
+bool lis2dw12ReadRegister_withlen(bitserial_handle handle, uint8 reg,  uint8 *value, uint8 len)
+{
+    bitserial_result result;
+    /* First write the register address to be read */
+    result = BitserialWrite(handle,
+                            BITSERIAL_NO_MSG,
+                            &reg, 1,
+                            BITSERIAL_FLAG_BLOCK);
+    if (result == BITSERIAL_RESULT_SUCCESS){
+        /* Now read the actual data in the register */
+        result = BitserialRead(handle,
+                                BITSERIAL_NO_MSG,
+                                value, len,
+                                BITSERIAL_FLAG_BLOCK);
+    }
+    if(result != BITSERIAL_RESULT_SUCCESS){
+        //printf("%s faild,result = %d\n",__func__, result);
+    }
+    return (result == BITSERIAL_RESULT_SUCCESS);
+}
+
+bitserial_handle lis2dw12Enable(void)
+{
+    uint16 bank;
+    uint32 mask;
+
+    bank = PIO2BANK(LIS2DW12_ITR_PIN);
+    mask = PIO2MASK(LIS2DW12_ITR_PIN);
+    PanicNotZero(PioSetMapPins32Bank(bank, mask, mask));
+    PanicNotZero(PioSetDir32Bank(bank, mask, 0));
+    PanicNotZero(PioSet32Bank(bank, mask, mask));
+
+    return hwi2cOpen(LIS2DW12_I2C_ADDR, LIS2DW12_I2C_FREQ);
+}
+
+void lis2dw12Disable(bitserial_handle handle)
+{
+    hwi2cClose(handle);
+}
+
+typedef struct{
+    uint8 reg;
+    uint8 mask;
+    uint8 value;
+}lis2dw12_str;
+lis2dw12_str lis2dw12_init_array[] = {
+    {0x25, 3<<4,    0<<4},//Set full scale, fs 0
+    {0x20, 0xf,     0},//Configure power mode, mode lp_mode 0
+    {0x25, 1<<2,    1<<2},//low_noise 1
+    {0x20, 0xf<<4,  7<<4},//odr
+    {0x22, 3,       0},//slp_mode 0
+    {0x22, 1<<5,    1<<5},//PP_OD 1
+    {0x22, 1<<4,    1<<4},//interrupt request latched
+    {0x22, 1<<3,    1<<3},//H_LACTIVE 1 //1: active low
+    {0x32, 1<<5,    1<<5},//tap_z_en 1
+    {0x32, 1<<6,    1<<6},//tap_y_en 1
+    {0x32, 1<<7,    1<<7},//tap_x_en 1
+    {0x30, 0x1f,    9},//tap_thsx 12
+    {0x31, 0x1f,    9},//tap_thsy 12
+    {0x32, 0x1f,    9},//tap_thsz 12
+    {0x33, 0xf<<4,  1<<4},//latency 7
+    {0x33, 3<<2,    3<<2},//quiet 3
+    {0x33, 3,       3},//shock 3
+    {0x34, 1<<7,    1<<7},//single_double_tap 1
+    {0x23, 1<<3,    1<<3},//int1_tap 1
+    {0x3f, 1<<5,    1<<5},//interrupts_enable 1
+};
+
+void lis2dw12_init(void)
+{
+    bitserial_handle handle;
+    uint8 value;
+    uint8 i;
+    value = 0;
+    handle = lis2dw12Enable();
+    if(BITSERIAL_HANDLE_ERROR == handle) {
+        return;
+    }
+    lis2dw12ReadRegister(handle, 0xf, &value);
+    lis2dw12ReadRegister(handle, 0xf, &value);
+    printf("lis2dw12 id = 0x%x\n", value);
+    if(value != 0x44){
+        printf("can not get lis2dw12 id!\n");
+        return;
+    }
+
+    for(i=0; i<ARRAY_DIM(lis2dw12_init_array); i++){
+        lis2dw12ReadRegister(handle,
+                lis2dw12_init_array[i].reg,
+                &value);
+        value &= ~lis2dw12_init_array[i].mask;
+        value |= lis2dw12_init_array[i].value;
+        lis2dw12WriteRegister(handle,
+                lis2dw12_init_array[i].reg,
+                value);
+    }
+    for(i=0; i<ARRAY_DIM(lis2dw12_init_array); i++){
+        lis2dw12ReadRegister(handle, lis2dw12_init_array[i].reg, &value);
+        printf("lis2dw12 reg 0x%x = 0x%x\n", lis2dw12_init_array[i].reg, value);
+    }
+
+    lis2dw12Disable(handle);
+    return;
+}
+
+#endif
---
+++ Earbuds1/apps/applications/earbud/tws/tap_func.c
@@ -0,0 +1,117 @@
+#include "bma400.h"
+#include "lis2dw12.h"
+#include <bitserial_api.h>
+#include <panic.h>
+#include <pio.h>
+#include <hydra_macros.h>
+#include <input_event_manager.h>
+
+typedef struct tagSHELLCMDINFO {
+    TaskData       task;
+}tapfuncInfoTask;
+static tapfuncInfoTask *pTapfuncTask = NULL;
+
+#ifdef HAVE_BMA400
+void tap_itr_handle_bma400(void)
+{
+    uint8 value_a[3];
+    bitserial_handle handle;
+    handle = BMA400Enable();
+    BMA400ReadRegister(handle, 0x0e, value_a+0);
+    BMA400ReadRegister(handle, 0x0f, value_a+1);
+    BMA400ReadRegister(handle, 0x10, value_a+2);
+    //printf("reg0x0e=0x%x, reg0x0f=0x%x, reg0x10=0x%x\n",value_a[0], value_a[1], value_a[2]);
+#ifdef MUTI_TAP
+    uint32 time;
+    if(value_a[1] & 0x4){
+        time = VmGetClock();
+        printf("single tap, time = %d\n", time);
+    }
+#endif
+#ifndef ONLY_DOUBLE_TAP
+    if(value_a[1] & 0x4){
+        printf("single tap\n");
+        MessageSend(&(appGetUi()->task), 1000, NULL);
+    }
+#endif
+#ifndef MUTI_TAP
+    if(value_a[1] & 0x8){
+        printf("double tap\n");
+        //MessageSend(&(appGetUi()->task), 1000, NULL);
+        MessageSend(&(appGetUi()->task), 1006, NULL);
+        CONN_RULES_BLE_CONNECTION_UPDATE_T *msg_value = PanicUnlessNew(CONN_RULES_BLE_CONNECTION_UPDATE_T);
+        msg_value->enable = TRUE;
+        MessageSend(&(appGetSm()->task), CONN_RULES_BLE_CONNECTION_UPDATE, msg_value);
+    }
+#endif
+    BMA400Disable(handle);
+}
+#endif
+
+#ifdef HAVE_LIS2DW12
+void tap_itr_handle_lis2dw12(void)
+{
+    //uint8 value;
+    uint8 value_arr[16];
+    bitserial_handle handle;
+    handle = lis2dw12Enable();
+    lis2dw12ReadRegister_withlen(handle, 0x37, value_arr, 5);
+//    printf("reg 0x37=0x%x, 0x38=0x%x, 0x39=0x%x, 0x3a=0x%x, 0x3b=0x%x\n",
+//           value_arr[0], value_arr[1], value_arr[2], value_arr[3], value_arr[4]);
+    if(value_arr[0] & 0x10){
+        printf("double tap\n");
+        //MessageSend(&(appGetUi()->task), 1000, NULL);
+    }
+    lis2dw12Disable(handle);
+    return;
+}
+#endif
+
+void tap_itr_handler(Task task, MessageId id, Message msg)
+{
+    //(void)id;(void)msg;(void)task;
+    (void)task;
+    unsigned long pin=0, pio_state;
+    MessagePioChanged *pioMsg = (MessagePioChanged*)msg;
+#ifdef HAVE_BMA400
+    pin = BMA400_ITR_PIN;
+#endif
+#ifdef HAVE_LIS2DW12
+    pin = LIS2DW12_ITR_PIN;
+#endif
+    pin = PIO2MASK(pin);
+    pio_state = pioMsg->state16to31 << 16 | pioMsg->state;
+    switch(id) {
+        case MESSAGE_PIO_CHANGED:
+            if( !(pin&pio_state) ){
+#ifdef HAVE_BMA400
+                    tap_itr_handle_bma400();
+#endif
+#ifdef HAVE_LIS2DW12
+                    tap_itr_handle_lis2dw12();
+#endif
+            }
+            break;
+        default:
+            printf("id=%d(0x%x\n", id, id);
+            break;
+    }
+}
+
+void tap_func_init(void)
+{
+    pTapfuncTask = PanicUnlessNew(tapfuncInfoTask);
+    memset(pTapfuncTask, 0, sizeof(tapfuncInfoTask));
+#ifdef HAVE_BMA400
+    BMA400_init();
+    pTapfuncTask->task.handler = tap_itr_handler;
+    InputEventManagerRegisterTask(&pTapfuncTask->task, BMA400_ITR_PIN);
+#endif
+#ifdef HAVE_LIS2DW12
+    lis2dw12_init();
+    pTapfuncTask->task.handler = tap_itr_handler;
+    InputEventManagerRegisterTask(&pTapfuncTask->task, LIS2DW12_ITR_PIN);
+#endif
+}
+
+
