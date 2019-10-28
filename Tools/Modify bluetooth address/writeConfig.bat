30dbe2660017e50d3ac8889874ef3b27cfef7122
qzj123 <2541872024@qq.com>
2019-10-28 15:26:24 +0800

    增加修改蓝牙地址文件夹

---
+++ Tools/Modify bluetooth address/writeConfig.bat
@@ -0,0 +1,5 @@
+::dump dev config 
+set BLUESUITE3="C:\Program Files (x86)\QTIL\BlueSuite 3.2.2"
+%BLUESUITE3%\ConfigCmd.exe -usbdbg 1 txt2dev system.htf REPLACE -system QCC512X_ROM_V21_CONFIG -database hydracore_config.sdb
+
+pause
