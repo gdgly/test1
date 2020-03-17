b1b47412a0ef5d1d03f7ffbff6aa46de11413b35
jisheng.he <wcl.hjs@gmail.com>
2020-03-17 15:11:05 +0800

    fix(电量状态): 修正电量状态为100时，手机不能及时显示问题

--- Earbuds1/apps/applications/earbud/tws/battery_status_notify.c
+++ Earbuds1/apps/applications/earbud/tws/battery_status_notify.c
@@ -25,6 +25,10 @@ typedef struct {
 
 BatteryStatusTaskData batteryStatusTaskData;
 
+enum {
+    INIT_NOTIFY_DEVICE_BATTERY = 0X100,
+};
+
 // endregion
 
 static void sendBatteryByAtCmd(uint8 percent) {
@@ -33,9 +37,9 @@ static void sendBatteryByAtCmd(uint8 percent) {
     if (NULL != data) {
         memcpy(data, batt_level_string, sizeof(batt_level_string));
         DEBUG_LOGF("AT: Send Cmd battery percent %d\n", percent);
-        data[BATTERY_POS] = (percent / 10) + 0x30;
+        data[BATTERY_POS] = ((percent - 1) / 10) + 0x30;
         DEBUG_PRINTF("AT Send:[%s]\n", data);
-//        HfpAtCmdRequest(hfp_primary_link, data);
+        HfpAtCmdRequest(hfp_primary_link, data);
         free(data);
     }
 }
@@ -54,6 +58,11 @@ static void batteryStatusNotifyHandleMessage(Task task, MessageId id, Message me
         case MESSAGE_BATTERY_LEVEL_UPDATE_PERCENT:
             batteryLevelUpdatePercent((MESSAGE_BATTERY_LEVEL_UPDATE_PERCENT_T *) message);
             break;
+
+        case INIT_NOTIFY_DEVICE_BATTERY:
+            sendBatteryByAtCmd(appBatteryGetPercent());
+            break;
+
         default:
             break;
     }
@@ -66,22 +75,32 @@ void batteryStatusNotifyInit(void) {
     batteryStatusTaskData.Client.task = &batteryStatusTaskData.task;
     batteryStatusTaskData.Client.hysteresis = 1;
     batteryStatusTaskData.Client.representation = battery_level_repres_percent;
-
     appBatteryRegister(&batteryStatusTaskData.Client);
 }
 
 void batteryStatusNotifyEnableReport(void) {
     DEBUG_PRINTF("AT Send:[%s]\n", batt_enable_string);
     HfpAtCmdRequest(hfp_primary_link, batt_enable_string);
+    sendBatteryByAtCmd(appBatteryGetPercent());
 }
 
 
+// region 扩展
 
+/*
+HFP命令AT+IPHONEACCEV
+        描述：报告耳机的状态变更
+        发起者：耳机
+        格式：AT+IPHONEACCEV=[Number of key/value pairs ],[key1 ],[val1 ],[key2 ],[val2 ],...
+参数：
+Number of key/value pairs : 接下来参数的数量
+        key: 被报告状态变化的类型
+1 = 电量等级
+2 = 暂停状态
+        val: 更改的值
+        Battery events:0-9之间数字的字符串 A string value between '0' and '9'.
+Dock state: 0 = undocked, 1 = docked.
+Example: AT+IPHONEACCEV=1,1,3
+*/
 
-
-
-
-
-
-
-
+// endregion
