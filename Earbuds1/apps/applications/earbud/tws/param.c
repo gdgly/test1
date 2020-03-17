d529537b79b72c3b16dac23bf66f521a9f0bfc23
jisheng.he <wcl.hjs@gmail.com>
2020-03-17 15:13:07 +0800

    build(01.02.36.01): 电话、点亮状态栏问题复测版本

--- Earbuds1/apps/applications/earbud/tws/param.c
+++ Earbuds1/apps/applications/earbud/tws/param.c
@@ -23,8 +23,8 @@
 
 
 
-/* SW版本共4字节： xx.01.01.23   xx为测试使用*/
-const uint8 SYSTEM_SW_VERSION[DEV_SWVER_LEN] = {0x00,0x01,0x02,0x35};   // 每次发布版本修改这儿的值
+/* SW版本共4字节： 01.01.23.xx   xx为测试使用*/
+const uint8 SYSTEM_SW_VERSION[DEV_SWVER_LEN] = {0x01,0x02,0x36,0x01};   // 每次发布版本修改这儿的值
 
 // 保存参数，如果length=0,表示擦除当前PSK中的参数
 static int16 ParamSave(uint16 pskey, void *buffer, int length)
